8c25bc5b3dfd478d1642c748ff54f7e0adebfae2
vellyariona <vellya.riona@mail.ugm.ac.id>
2023-05-18 18:03:40 +0700

    20230518

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,38 @@
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<title>Thrifttopia</title>
 
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Italiana">
+		<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Montserrat">
+		<script src="https://kit.fontawesome.com/88ef642845.js" crossorigin="anonymous"></script>
+
 		<link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 		<link href="{{ asset('/css/thrifttopia.css') }}" rel="stylesheet">
 	</head>
 	<body>
 
 		{{-- navbar --}}
-		<section class="navbar-background">
-			@include('layouts.navbar')
-		</section>
+		<div class="navbar-background">
+			<section id="jumbotron">
+				<div class="jumbotron mx-auto pt-5">
+				<div class="container">
+					<div class="row">
+					<div class="col-8">
+						<h1 class="display-4" style="font-family: Italiana;">Jual barang bekas anti ribet <br>
+						hanya di 
+						<img src="{{ asset('images/icons/thriftopia-blue.svg') }}"> 
+						</h1>
+						<p class="lead">
+						<button class="mt-2 pl-4 pr-4 btn btn-outline-dark" href="#" role="button" style="border-radius: 20px;">Coba Sekarang!</button>
+						</p>
+					</div>
+					<div class="col customimage">
+						<img src="{{ asset('images/pictures/jumbotron.png') }}" alt="thriftopia" max-height="500">
+					</div>
+				</div>
+				</div>
+			</section>
+		</div>
+		@include('layouts.navbar')
 		
 		{{-- content --}}
 		<div class="page-content">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -11,3 +11,23 @@
         </li>
     </ul>
 </nav>
+
+
+
+
+{{-- 
+<section id="navbar">
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <a class="navbar-brand" href="#">
+          <img src="{{ asset('/images/icons/thrifttopia_icons.svg ') }}" alt="thriftopia" width="200" height="40">
+        </a>
+        <ul class="nav justify-content-end text-end">
+          <li class="nav-item">
+            <a class="nav-link" href="#tentang">Tentang</a>
+          </li>
+          <li class="nav-item" id="masuk">
+            <a class="nav-link" href="#masuk" >Masuk</a>
+          </li>
+        </ul>
+    </nav>
+</section> --}}
