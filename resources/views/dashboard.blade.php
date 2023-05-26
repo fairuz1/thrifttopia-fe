b290811005cbea5950e7c4b74142bdc023bce631
fairuz1 <akbarazariafairuz@gmail.com>
2023-05-26 07:50:49 +0700

    20230526

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
     <div class="row mx-auto">
-
+        {{-- search bar --}}
         <div class="dashboard-search input-group mb-3 px-5">
             <button class="btn btn-outline-secondary dropdown-toggle px-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">Semua Kategori</button>
             <ul class="dropdown-menu">
@@ -137,14 +137,14 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
@@ -159,14 +159,14 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
@@ -181,14 +181,14 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
@@ -203,14 +203,36 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
+                                    <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
+                                </button> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            
+            <div class="col-2 p-2">
+                <div class="card">
+                    <div class="d-flex justify-content-center align-items-end p-4">
+                        <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
+                        <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
+                    </div>
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
+                                <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
+                                <p class="card-text">Rp 50.000</p>
+                            </div>
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
@@ -225,14 +247,14 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
@@ -247,14 +269,36 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
+                                    <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
+                                </button> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            
+            <div class="col-2 p-2">
+                <div class="card">
+                    <div class="d-flex justify-content-center align-items-end p-4">
+                        <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
+                        <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
+                    </div>
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
+                                <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
+                                <p class="card-text">Rp 50.000</p>
+                            </div>
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
@@ -269,14 +313,14 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
@@ -291,14 +335,36 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
+                                    <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
+                                </button> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            
+            <div class="col-2 p-2">
+                <div class="card">
+                    <div class="d-flex justify-content-center align-items-end p-4">
+                        <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
+                        <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
+                    </div>
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
+                                <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
+                                <p class="card-text">Rp 50.000</p>
+                            </div>
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
@@ -313,80 +379,14 @@
                         <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
                         <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
+                    <div class="card-body pt-0">
+                        <div class="row px-4">
+                            <div class="col-9 px-0">
                                 <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
                                 <p class="card-text">Rp 50.000</p>
                             </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
-                                    <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
-                                </button> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>  
-
-            <div class="col-2 p-2">
-                <div class="card">
-                    <div class="d-flex justify-content-center align-items-end p-4">
-                        <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
-                        <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
-                                <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
-                                <p class="card-text">Rp 50.000</p>
-                            </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
-                                    <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
-                                </button> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>  
-
-            <div class="col-2 p-2">
-                <div class="card">
-                    <div class="d-flex justify-content-center align-items-end p-4">
-                        <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
-                        <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
-                                <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
-                                <p class="card-text">Rp 50.000</p>
-                            </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
-                                    <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
-                                </button> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>  
-
-            <div class="col-2 p-2">
-                <div class="card">
-                    <div class="d-flex justify-content-center align-items-end p-4">
-                        <div class="post-status py-1 px-3"><p class="mb-0">ELEKTRONIK</p></div>
-                        <img src="{{ asset('images/pictures/empty-image.png') }}" class="card-img-top pt-3">
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
-                                <h5 class="card-title font-weight-bold"><b>Radio 1990</b></h5>
-                                <p class="card-text">Rp 50.000</p>
-                            </div>
-                            <div class="col-2">
-                                <button class="btn shadow-none">
+                            <div class="col-3 d-flex justify-content-end px-0">
+                                <button class="btn shadow-none p-0">
                                     <img src="{{ asset('images/icons/arrow-circle-right.svg') }}" alt="go">
                                 </button> 
                             </div>
